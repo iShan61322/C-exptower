bafbd4267663673365c0f4dee8a1e6742c99ff29
ToAtob <127174664+iShan61322@users.noreply.github.com>
2023-06-28 14:13:26 +0800

    Add files via upload

---
+++ 数组逆序.c
@@ -0,0 +1,49 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+
+//实现init()函数初始化数组为0
+//实现print()函数打印数组
+//实现reverse()函数实现数组逆序
+
+void init(int arr[], int sz)
+{
+	int i = 0;
+	for (i = 0; i < sz; i++)
+	{
+		arr[i] = 0;
+	}
+}
+void print(int arr[], int sz)
+{
+	int i = 0;
+	for (i = 0; i < sz; i++)
+	{
+		printf("%d ", arr[i]);
+		
+	}
+	printf("\n");
+}
+void reverse(int arr[], int sz)
+{
+	int left = 0;
+	int right = sz - 1;
+	while (left < right)
+	{
+		int tmp = arr[left];
+		arr[left] = arr[right];
+		arr[right] = tmp;
+		left++;
+		right--;
+	}
+}
+int main()
+{
+	int arr[] = { 1,2,3,4,5,6,7,8,9,10 };
+	int sz = sizeof(arr) / sizeof(arr[0]);
+	print(arr, sz);
+	reverse(arr, sz);
+	print(arr, sz);
+	init(arr,sz);
+	print(arr, sz);
+	return 0;
+}
