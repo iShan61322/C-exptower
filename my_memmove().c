1dcf49ad6b157a06127de007b71c47e5af8fc3c5
voidboe <127174664+iShan61322@users.noreply.github.com>
2023-11-08 16:50:05 +0800

    Add files via upload

---
+++ my_memmove().c
@@ -0,0 +1,33 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+#include<assert.h>
+void* my_memmove(void* dest, const void* src, size_t num)
+{
+	void* ret = dest;
+	assert(dest && src);
+	//dest<src front->behind
+	if (dest < src)
+	{
+		while (num--)
+		{
+			*(char*)dest = *(char*)src;
+			dest = (char*)dest + 1;
+			src = (char*)src + 1;
+		}
+	}
+	//dest>src behind->front
+	else
+	{
+		while (num--)
+		{
+			*((char*)dest + num) = *((char*)src + num);
+		}
+	}
+	return ret;
+}
+int main()
+ {
+	int arr1[10] = {1,2,3,4,5,6,7,8,9,10};
+	my_memmove(arr1, arr1+2, 20);
+	return 0;
+}
